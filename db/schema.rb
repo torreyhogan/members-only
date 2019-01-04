e6875ecdcd329c400746d0792d76d8987680389e
Torrey Hogan <hogantorrey@gmail.com>
2019-01-03 16:22:50 -0800

    fully functional users with sessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_31_221552) do
+ActiveRecord::Schema.define(version: 2019_01_03_200045) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_12_31_221552) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
